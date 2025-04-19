a5b0fa95e761d62b1e4ddcacd80b0787745b1948
0seconds <5dudtj@Naver.com>
2025-04-19 16:34:10 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,18 @@ import { svelteTesting } from '@testing-library/svelte/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	resolve: {
+		alias: {
+			'p5': path.resolve('./node_modules/p5/lib/p5.min.js')
+		}
+	},
+	optimizeDeps: {
+		include: ['p5']
+	},
 	test: {
 		workspace: [
 			{
